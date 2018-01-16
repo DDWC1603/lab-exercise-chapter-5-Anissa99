3eaa7bb75b402d04500d7966eed3345ecb38e102
Anissa99 <33884218+Anissa99@users.noreply.github.com>
2018-01-16 10:14:58 +0800

    Task completed

--- Exercise-Function-1.cpp
+++ Exercise-Function-1.cpp
@@ -1,32 +1,33 @@
+//Nurul Anissa binti Huzaini
+//A17DW4087
 #include <iostream>
-
-
-
-int main()
-{
-
-}
+#include <conio.h>
+using namespace std;
 
 void masuk_tak()
 {
-	char ans;
-	
-	cout<<endl<<"Dia shoot masuk tak? (answer y-yes atau n-tidak"<<endl;
-	
-
+	char ans, c;
+	cout<<endl<<"Dia shoot masuk tak? (answer y-yes atau n-tidak)"<<endl;
 	ans=' ';
-	while(c!='y' and c!='y'){
-		cin>>ans;
-		if(c!='n' and c!='n')
-		cout<<endl<<"cakap lah bebetul, try again: "<<endl;
-		}
+
+	while(c!='y' and c!='n')
+    {
+	 cin>>c;
+	 if(c!='y' and c!='n')
+	 cout<<endl<<"cakap lah bebetul, try again: "<<endl;
+    }
 
 	if(c=='n')
 	{
-		cout<<endl<<"the fudge?!";
-		masuk_tak();
+	 cout<<endl<<"the fudge?!";
+	 masuk_tak();
 	}
 	cout<<endl<<"chantek!"<<endl;
 }
 
+int main()
+{
+ masuk_tak();
+ getch();
+}
 
