2881ffea060476f9ada0b7155887964e216b1a0b
Anissa99 <33884218+Anissa99@users.noreply.github.com>
2018-01-16 10:49:37 +0800

    Task completed

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -1,9 +1,24 @@
-//this is an unfinish program
-//finish up this program
+//Nurul Anissa binti Huzaini
+//A17DW4087
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int sum(int x,int y);
 
-int sum(int x,int y)
+int main()
 {
 	int result;
+	char x,y;
+	
+	x=20;
+	y=10;
+	
+	cout<< "x = 20"<<endl;
+	cout<< "y = 10"<<endl;
+	
 	result = x+y;
+	
+	cout<<"The result is: "<<result<<endl;
+	getch();
 
 }
